233ee899bd7a42eb2bbd075f3cab93b3810243a9
Kolisnyk <Kolisnyk@inbox.ru>
2016-12-14 14:51:59 +0000

    Pre-staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server ENV['PRODUCTION_IP'] || '139.59.140.162', port: 22,
+server ENV['PRODUCTION_IP'] || '46.101.201.125', port: 22,
                                                  roles: [:web, :app, :db],
                                                  primary: true
 
